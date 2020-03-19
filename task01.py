cc82b36703f458a874d331a3a4d6075ea92e11e6
name <name@mail>
2020-03-19 15:54:57 +0200

    11111

---
+++ task01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.7
+
+a = input("enter interger value for a: ")
+b = input("enter interger value for b: ")
+
+a = int(a)
+b = int(b)
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+name     = input("what is your name? ")
+age      = input("how old are you? ")
+location = input("where do you live? ")
+
+print("Hello ", name, ", your age is ", age, ", you live in ", location)
