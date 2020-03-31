04d26fbfc5f10dfd1886d3f8cc67971fee189c2c
petro <email>
2020-03-31 18:26:02 +0300

    work 5

--- task05.py
+++ task05.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.7
 
 import sys
+from task01 import get_integer
 
 def success_traver(distance):
     rate = 25
@@ -12,6 +13,7 @@ def enough(ca, on, wait):
     return 1 if (ca - on) >= wait else 0
 
 def banjo():
+    print('\ntesting name')
     name = input('Are you playing banjo? Enter your name: ')
     if len(name) == 0:
         exit("error: you doesn't provide name!")
@@ -24,9 +26,11 @@ def banjo():
     return name + msg
 
 def yes_no(val):
+    print('\ntesting Yes No')
     return 'Yes' if val else 'No'
 
 def sheep(sheeps):
+    print('\ncounting sheeps')
     count = 0
 
     for sheep in sheeps:
@@ -35,6 +39,7 @@ def sheep(sheeps):
     return count
 
 def correct_tail(body, tail):
+    print('testing tail in', body, tail)
     if len(tail) > len(body):
         sys.exit("error: tail bigger than body")
     
@@ -44,6 +49,40 @@ def correct_tail(body, tail):
         return True
     else:
         return False
+
+def get_min_max():
+    print('testing 3 number for min and max')
+    values = []
+
+    for i in range(3):
+        values.insert(i, get_integer("enter integer value: "))
+
+    print('result is min = {}, max {}'.format(min(values), max(values)))
+    
+def what_numbers():
+    print('testing values from 1 to 11')
+
+    for i in range(1,11):
+        if i % 2 == 0:
+            print(i, 'even value')
+        elif i % 3 == 0:
+            print(i, 'multiple 3')
+        else:
+            print(i, 'not multiple 2 or 3')
+
+def fact(number):
+    # result = 1
+
+    # for i in range(1, number + 1):
+    #     result *= i
+
+    result, i = 1, 1
+    while i <= number:
+        result *= i
+        i += 1
+
+    return result
+
 
 if __name__ == "__main__":
     #---------------------------------------------------------------------------
@@ -77,3 +116,19 @@ if __name__ == "__main__":
     # tail vs body
     print(correct_tail('Fox', 'x'))
     print(correct_tail('Emu', 't'))
+
+    #---------------------------------------------------------------------------
+    # min max in range
+    print()
+    get_min_max()
+    
+    #---------------------------------------------------------------------------
+    # testing numbers
+    print()
+    what_numbers()
+
+    #---------------------------------------------------------------------------
+    # get fact
+    number = 5
+    print('\nfactorial of', number, 'is', fact(number))
+
